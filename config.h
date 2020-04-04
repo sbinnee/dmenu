54791e2b9d857e3f0f00217d7dc7ee0ed0283a19
Seongbin Lim <sungbin246@gmail.com>
2020-04-04 21:12:28 +0900

    Set default font to 14, smaller than emoji&icons

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"DejaVu Sans Mono:size=18",
+	"DejaVu Sans Mono:size=14",
 	"JoyPixels:size=18",
 	"Symbols Nerd Font:size=18"
 	/* "monospace:size=10", */
