cadd5a0158e50592887b17151c29707a74cacd13
Seongbin Lim <sungbin246@gmail.com>
2020-04-07 11:10:04 +0900

    Orange is new black

--- config.h
+++ config.h
@@ -13,11 +13,19 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#d79921", "#005577" },
+	[SchemeSel] = { "#eeeeee", "#d79921" },
+	[SchemeSelHighlight] = { "#222222", "#d79921" },
 	[SchemeNormHighlight] = { "#d79921", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+// static const char *colors[SchemeLast][2] = {
+	// [>     fg         bg       <]
+	// [SchemeNorm] = { "#bbbbbb", "#222222" },
+	// [SchemeSel] = { "#eeeeee", "#005577" },
+	// [SchemeSelHighlight] = { "#d79921", "#005577" },
+	// [SchemeNormHighlight] = { "#d79921", "#222222" },
+	// [SchemeOut] = { "#000000", "#00ffff" },
+// };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
