c757e5ae7cf620cc68178caa44f716fc5e9efe38
Seongbin Lim <sungbin246@gmail.com>
2021-12-06 14:29:19 +0100

    Apply dracula colorscheme

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 1200;                    /* minimum width when centered */
+static int topbar = 1;						/* -b  option; if 0, dmenu appears at bottom	 */
+static int centered = 0;					/* -c option; centers dmenu on screen */
+static int min_width = 1200;					/* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"DejaVu Sans Mono:size=14",
@@ -11,17 +11,17 @@ static const char *fonts[] = {
 	"Symbols Nerd Font:size=18"
 	/* "monospace:size=10", */
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt	   = NULL;		/* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#d79921" },
-	[SchemeSelHighlight] = { "#222222", "#d79921" },
-	[SchemeNormHighlight] = { "#d79921", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*	   fg		  bg	   */
+	[SchemeNorm] = { "#f8f8f2", "#282a36" },
+	[SchemeSel] = { "#282a36", "#bd93f9" },
+	[SchemeSelHighlight] = { "#f8f8f2", "#bd93f9" },
+	[SchemeNormHighlight] = { "#bd93f9", "#282a36" },
+	[SchemeOut] = { "#000000", "#8be9fd" },
 };
 // static const char *colors[SchemeLast][2] = {
-	// [>     fg         bg       <]
+	// [>	  fg		 bg		  <]
 	// [SchemeNorm] = { "#bbbbbb", "#222222" },
 	// [SchemeSel] = { "#eeeeee", "#005577" },
 	// [SchemeSelHighlight] = { "#d79921", "#005577" },
@@ -29,7 +29,7 @@ static const char *colors[SchemeLast][2] = {
 	// [SchemeOut] = { "#000000", "#00ffff" },
 // };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines	   = 0;
 
 /*
  * Characters not considered part of a word while deleting words
